69e0305cef9a5e64c87c76afdd42b789bff3da51
Mason Mann <masonandrewmann@gmail.com>
2020-12-11 19:15:24 -0500

    webserver inputs for DMX address and universe

--- webserver_input/webserver_input.ino
+++ webserver_input/webserver_input.ino
@@ -22,6 +22,9 @@ float   h = 0 ;
 float   p = 0;
 String  LEDstate = "OFF";
 
+int universeNum = 0;
+int dmxAdr = 0;
+
 //create Objects
 //Adafruit_BMP280 bmp; // I2C
 ESP8266WebServer server ( 80 );
@@ -29,19 +32,19 @@ ESP8266WebServer server ( 80 );
 String getPage(){
   //String page = "<html lang=fr-FR><head><meta http-equiv='refresh' content='10'/>";
   String page = "<html lang=fr-FR><head>";
-  page += "<title>ESP8266</title>";
+  page += "<title>Light Bar</title>";
   page += "<style> body { background-color: #fffff; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }</style>";
-  page += "</head><body><h1>ESP8266 Demo</h1>";
-  page += "<h3>bmp280</h3>";
-  page += "<ul><li>Temperature:";
-  page += t;
-  page += "°C</li>";
-  page += "<li>Pressure: ";
-  page += p;
-  page += " mbar</li></ul>";
+  page += "</head><body><h1>DMX Configuration</h1>";
+  page += "<h3>Universe</h3>";
   page += "<form action='/submit' method='POST'>";
-  page += "<ul><li><INPUT type='number' name='Tset' id='Tset' value='24' min='15' max='24'>";
-  page += "<INPUT type='submit' value='Submit'></li></ul>";
+  page += "<ul><li><input type='number' name='Uni' id='Uni' value='" + String(universeNum) + "' min='0' max='24'>";
+  page += "</li></ul>";
+  page += "<br>";
+  page += "<h3>Address</h3>";
+  page += "<form action='submit' method='POST'>";
+  page += "<ul><li><input type='number' name='Adr' id = 'Adr' value ='" + String(dmxAdr) + "' min='0' max='24'>";
+  page += "<br><br><br>";
+  page += "<input type = 'submit' value='Submit'></li></ul>";
   page += "</body></html>";
   return page;
 }
@@ -56,7 +59,11 @@ void handleRoot(){
 void handleSubmit(){
 
   Serial.println ("handleSubmit");
-    String Tsetarduino = server.arg(0);
+    String universeNumStr = server.arg(0);
+    String dmxAdrStr = server.arg(1);
+
+    universeNum = universeNumStr.toInt();
+    dmxAdr = dmxAdrStr.toInt();
    
   String
   message = "URI: ";
@@ -70,8 +77,10 @@ void handleSubmit(){
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   Serial.println(message);
-  Serial.println("Variable passed:");
-  Serial.println(Tsetarduino);
+  Serial.println("Universe Number:");
+  Serial.println(universeNum);
+  Serial.println("DMX Address:");
+  Serial.println(dmxAdr);
   server.send ( 200, "text/html", getPage() );
 }
 
